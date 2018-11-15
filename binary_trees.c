670cbab80657f169165481109f354435b6aeebfd
SysVic <wd.victorhugo@gmail.com>
2018-11-15 13:35:15 -0200

    addnotes partially finalized

--- binary_trees.c
+++ binary_trees.c
@@ -19,19 +19,37 @@ int main(int argc, char const *argv[])
 	return 0;
 }
 void addnode(No * root, int number){
+	//check if exist some root node
 	if(root == NULL){
 		root = (No *) malloc(sizeof(No));
 		root->right = NULL;
 		root->left = NULL;
 		return;
-	}else if( number > root->number){
+	}else if( number < root->number){
+		if(root->left == NULL){
+			//if the right node is empty, just put the value there
+			root->left = (No *) malloc(sizeof(No));
+			root->left->number = number;
+			root->left->right = NULL;
+			root->left->left = NULL;
+			return;
+		}else{
+			//other wise repeat the step
+			addnode(root->left , number);
+			return;
+		}
+
+	}else{
+		//go to right subtree if the number is greater or equal than root value
 		if(root->right == NULL){
+			//if the right node is empty, just put the value there
 			root->right = (No *) malloc(sizeof(No));
 			root->right->number = number;
 			root->right->right = NULL;
 			root->right->left = NULL;
 			return;
 		}else{
+			//other wise repeat the step
 			addnode(root->right , number);
 			return;
 		}
